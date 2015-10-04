6a0c943f37c40580613042177705378e6148fbf4
Alexander Herrmann <darignac@gmail.com>
2015-10-04 14:45:04 +0200

    added batch script to easily create the mod zip archive in the mods folders

---
+++ copy.bat
@@ -0,0 +1,1 @@
+"%PROGRAMFILES%\7-Zip\7z.exe" a -r "%HOMEPATH%\Documents\my games\FarmingSimulator2015\mods\HarvesterStatus.zip" "%HOMEPATH%\Documents\GitHub\fs15-harvesters\*.lua" "%HOMEPATH%\Documents\GitHub\fs15-harvesters\*.xml" "%HOMEPATH%\Documents\GitHub\fs15-harvesters\*.png"
